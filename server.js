8966e0ab460e122c0892dcdc6d77452e99f13a9a
Zuleiha Dantijani <tzuleiha@gmail.com>
2021-07-10 19:27:49 +0100

    new commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+const color = require('./src/util/color')
+
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use('/', (req, res) => {
+  res.send('Hey Instaclone!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`server is running at port ${PORT}`.success));
---
+++ src/util/color.js
@@ -0,0 +1,14 @@
+const colors = require('colors');
+
+module.exports = colors.setTheme({
+  success: 'rainbow',
+  input: 'grey',
+  verbose: 'cyan',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'red',
+});
